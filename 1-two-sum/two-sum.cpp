7c898e917a26244959a37081d582ec302f64b99f
Hugo Rodrigues Torquato <67749317+HugorTorquato@users.noreply.github.com>
2024-10-28 12:08:08 -0300

    Time: 3 ms (80.15%) | Memory: 14.6 MB (14.34%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        map<int, int> hashSearch;
+        int index = 0;
+
+        for(int num : nums){
+            int valueToEvaluate = target - num;
+            if(hashSearch.find(valueToEvaluate) != hashSearch.end()){
+                return {hashSearch[valueToEvaluate], index};
+            } else {
+                hashSearch[num] = index;
+            }
+            index++;
+        }
+        return {0, 0};
+
+        /*
+
+        //O(N^2)
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target && i != j){
+                    indexResults.push_back(i);
+                    indexResults.push_back(j);
+                    return indexResults;
+                }
+            }
+        }
+
+        return indexResults;
+        
+        */
+    }
+};
