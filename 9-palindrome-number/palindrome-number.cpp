90fbf24d6434d8ec249858e47fcb0464a9e0594e
Hugo Rodrigues Torquato <67749317+HugorTorquato@users.noreply.github.com>
2024-10-30 12:02:24 -0300

    Time: 8 ms (15.04%) | Memory: 8.3 MB (89.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,39 +2,19 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        if(x < 0) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
-        bool stillHasNumbers = true;
-        vector<int> numberXAsVector;
-        int processedNumber = x;
+        int reverseNumber = 0;
 
-
-        while(stillHasNumbers){
-            if(processedNumber > 0){
-                numberXAsVector.push_back(processedNumber % 10);
-                processedNumber /= 10;               
-            } else {
-                stillHasNumbers = false;
-            }
+        while (x > reverseNumber){
+            reverseNumber *= 10;
+            reverseNumber += x % 10;
+            x /= 10;
         }
 
-        
-
-        while(numberXAsVector.size() > 1){
-
-            if(numberXAsVector.front() != numberXAsVector.back()){
-                return false;
-            }
-
-            numberXAsVector.erase(numberXAsVector.begin());
-            numberXAsVector.erase(numberXAsVector.end());
-
-             
-        }
-
-        return true;
+        return x == reverseNumber || x == reverseNumber / 10;
         
     }
 };
